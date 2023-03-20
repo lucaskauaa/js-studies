db2349f3b63e3cda04c88c9481c4df82a3d2502f
Lucas Kauã <104635659+lucaskauaa@users.noreply.github.com>
2023-03-20 10:06:17 -0300

    Update arr-methods03.js

--- study009/arr-methods03.js
+++ study009/arr-methods03.js
@@ -30,3 +30,26 @@ const numbers03 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const testNumbers = numbers03.filter(n => n % 2 === 0).map(n => n * 2);
 
 console.log(testNumbers);
+
+
+const yetAnotherArray = [2, 3, 4, 5, 4, 12, 19, 7, 2, 5];
+
+const uniqueArray = yetAnotherArray.filter((value, index, arr) => {
+    return arr.indexOf(value) === index;
+});
+
+console.log(uniqueArray);
+
+
+const people = [
+    { id: 11, name: 'Adamastor', age: 23, group: 'editor' }, 
+    { id: 47, name: 'Joana', age: 28, group: 'user' }, 
+    { id: 85, name: 'Mauricio', age: 34, group: 'editor' }, 
+    { id: 97, name: 'Lalau', age: 74, group: 'admin' }
+];
+
+const filteredPeople = people.filter((value) => {
+    return value.name.includes('au');
+});
+
+console.log(filteredPeople);
