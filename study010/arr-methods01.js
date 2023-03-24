11af8f7d36cc5e2b853011a24e321c524f7a52cf
Lucas Kauã <104635659+lucaskauaa@users.noreply.github.com>
2023-03-23 23:14:28 -0300

    Create study010

---
+++ study010/arr-methods01.js
@@ -0,0 +1,39 @@
+// const fname = 'Lucas';
+
+// const arrName = Array.from(fname);
+
+// console.log(arrName);
+
+
+// const letters = ['a', 'b', 'c', 'd'];
+
+// const test = letters.keys();
+
+// for(let i of test) {
+//     console.log(i);
+// }
+
+
+// const letters02 = ['a', 'b', 'c', 'd'];
+
+// const test02 = letters02.values();
+
+// for(let i of test02) {
+//     console.log(i);
+// }
+
+
+// const fruits = ['Maça', 'Uva', 'Banana', 'Laranja'];
+
+// const test03 = fruits.entries();
+
+// for(let i of test03) {
+//     console.log(i);
+// }
+
+
+const fullName = ['Lucas', 'Kauã', 'Neves', 'de', 'Almeida', 'da', 'Costa'];
+
+const test04 = fullName.copyWithin(4, 0, 3);
+
+console.log(test04);
